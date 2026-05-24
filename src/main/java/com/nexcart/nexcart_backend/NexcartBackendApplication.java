5060102067b39511a134f8e710dc0f493b48fbf4
karthik23-cell <nayakunikarthik2306@gmail.com>
2026-05-24 21:55:56 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/nexcart/nexcart_backend/NexcartBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nexcart.nexcart_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexcartBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexcartBackendApplication.class, args);
+	}
+
+}
